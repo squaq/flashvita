50cc8c6553a32bbd113d8d9830ba71b8e1d1a06a
TissTech - Danilo S <tisstech@Mac-mini-de-TissTech.local>
2016-08-12 16:31:08 -0300

    new Auth

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,8 @@ angular.module('flashvitaApp')
     var cId = '16c9557d66fe46a2ba0c42a94d62a89a',
         cSec = '811fefc5d24e49189e243bbb9c444baa',
         redirect_uri = 'http://localhost:9000', 
-        url_api = 'http://localhost/~squaq/api.php';//'http://localhost/api.php';//
+        url_api = 'http://localhost/~squaq/api.php',
+        masterToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfbWV0YWRhdGEiOnsibmFtZSI6IkZlbGlwZSBMaW1hIiwicGljdHVyZSI6Imh0dHBzOi8vcy5ncmF2YXRhci5jb20vYXZhdGFyL2I3NDRmOGFkNzdmZTMyN2FhZjI4Y2JjM2RlZDA0YmE2P3M9NDgwJnI9cGcmZD1odHRwcyUzQSUyRiUyRmNkbi5hdXRoMC5jb20lMkZhdmF0YXJzJTJGZmUucG5nIiwiaW1hZ2VzIjp7ImxvZ28iOiIifSwiY25waiI6IiIsInBob25lIjoiIiwiY29udGFjdCI6IiIsImlzUGFydG5lciI6dHJ1ZSwicm9sZXMiOlsic3lzdGVtLmFjY2VzcyIsInN5c3RlbS5hZG1pbiIsIm1pc3Npb24uYXBwcm92ZXIiLCJhY2Nlc3MuaG9va3MiXSwib3JnYW5pemF0aW9uIjoiNTY2MDVjZmFkNGM2MDZjODVlYjEwZGQ2In0sImF1ZCI6ImkyUGNMVEx3N2dNS1dUMkg1TlNTYm9zUEpoMFMzbHFhIiwiaXNzIjoic3F1aWQuYXV0aDAuY29tIiwic3ViIjoiYXV0aDB8NTdhY2JlOTc5NDY2ZTA3NDcyNmMyOWMzIn0.8U5u4_caQv_HaZpqg_uBXnhA3omkFTJLgIb6jrogoyA';//'http://localhost/api.php';//
 //    $window.localStorage.setItem(key,value);
 //    $window.localStorage.removeItem('fvcode');
 //    $window.localStorage.removeItem('fvtoken');
@@ -96,6 +97,19 @@ angular.module('flashvitaApp')
     }
     
     $scope.authenticate = function(provider) {
-        window.location = 'https://api.instagram.com/oauth/authorize/?client_id='+cId+'&redirect_uri='+redirect_uri+'&response_type=code';
+        console.log("aqui!!");
+        var config = {headers : {
+            'Authorization': masterToken
+        }};
+        $http.get('https://integration.squidit.com.br/v1/monitoring', config)
+        .then(
+            function(s){
+                console.log('success',s)
+            }, 
+            function(e){
+                console.log('error', e);
+            }
+        );
+//        window.location = 'https://api.instagram.com/oauth/authorize/?client_id='+cId+'&redirect_uri='+redirect_uri+'&response_type=code';
     };
   });
